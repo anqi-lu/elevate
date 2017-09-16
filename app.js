acc1f312f69d4d71ab40796b9d219594a9203675
Yang Liu <byliuyang11@gmail.com>
2017-09-16 19:15:25 -0400

    Add command handlers

--- app.js
+++ app.js
@@ -134,10 +134,6 @@ app.get('/authorize', function (req, res) {
     var redirectURISuccess = redirectURI + "&authorization_code=" + authCode;
 });
 
-app.get('/test', (req, res) => {
-    sendTextMessage(1684435621589773, 'Hi!!!!');
-});
-
 /*
  * Verify that the callback came from Facebook. Using the App Secret from
  * the App Dashboard, we can verify the signature that is sent with each
@@ -221,7 +217,7 @@ function receivedMessage(event) {
         senderID, recipientID, timeOfMessage);
     console.log(JSON.stringify(message));
 
-    var isEcho = message.is_echo;
+    // var isEcho = message.is_echo;
     var messageId = message.mid;
     var appId = message.app_id;
     var metadata = message.metadata;
@@ -229,86 +225,109 @@ function receivedMessage(event) {
     // You may get a text or attachment but not both
     var messageText = message.text;
     var messageAttachments = message.attachments;
-    var quickReply = message.quick_reply;
+    // var quickReply = message.quick_reply;
 
-    if (isEcho) {
-        // Just logging message echoes to console
-        console.log("Received echo for message %s and app %d with metadata %s",
-            messageId, appId, metadata);
-        return;
-    } else if (quickReply) {
-        var quickReplyPayload = quickReply.payload;
-        console.log("Quick reply for message %s with payload %s",
-            messageId, quickReplyPayload);
+    // if (isEcho) {
+    //     // Just logging message echoes to console
+    //     console.log("Received echo for message %s and app %d with metadata %s",
+    //         messageId, appId, metadata);
+    //     return;
+    // } else if (quickReply) {
+    //     var quickReplyPayload = quickReply.payload;
+    //     console.log("Quick reply for message %s with payload %s",
+    //         messageId, quickReplyPayload);
+    //
+    //     sendTextMessage(senderID, "Quick reply tapped");
+    //     return;
+    // }
 
-        sendTextMessage(senderID, "Quick reply tapped");
-        return;
+    const handlers = [
+        {
+            command: /^buy ([0-9 a-zA-Z])$/,
+            action: (stockName) => {
+                sendTextMessage(senderID, `Ha ha ha #{senderID}, #{stockName}`);
+            }
+        }
+    ];
+
+    for(let handler in handlers) {
+        let results = messageText.match(handler.command);
+        if(results) {
+            let params = results.slice(1);
+            handler.action.apply(this, params);
+            return;
+        }
     }
 
-    if (messageText) {
+    sendTextMessage(senderID, messageText);
 
+
+    // if (messageText) {
+    //     switch (messageText) {
+    //         case ''
+    //     }
         // If we receive a text message, check to see if it matches any special
         // keywords and send back the corresponding example. Otherwise, just echo
         // the text we received.
-        switch (messageText) {
-            case 'image':
-                sendImageMessage(senderID);
-                break;
-
-            case 'gif':
-                sendGifMessage(senderID);
-                break;
-
-            case 'audio':
-                sendAudioMessage(senderID);
-                break;
-
-            case 'video':
-                sendVideoMessage(senderID);
-                break;
-
-            case 'file':
-                sendFileMessage(senderID);
-                break;
-
-            case 'button':
-                sendButtonMessage(senderID);
-                break;
-
-            case 'generic':
-                sendGenericMessage(senderID);
-                break;
-
-            case 'receipt':
-                sendReceiptMessage(senderID);
-                break;
-
-            case 'quick reply':
-                sendQuickReply(senderID);
-                break;
-
-            case 'read receipt':
-                sendReadReceipt(senderID);
-                break;
-
-            case 'typing on':
-                sendTypingOn(senderID);
-                break;
-
-            case 'typing off':
-                sendTypingOff(senderID);
-                break;
-
-            case 'account linking':
-                sendAccountLinking(senderID);
-                break;
-
-            default:
-                sendTextMessage(senderID, messageText);
-        }
-    } else if (messageAttachments) {
-        sendTextMessage(senderID, "Message with attachment received");
-    }
+        // switch (messageText) {
+        //     case 'image':
+        //         sendImageMessage(senderID);
+        //         break;
+        //
+        //     case 'gif':
+        //         sendGifMessage(senderID);
+        //         break;
+        //
+        //     case 'audio':
+        //         sendAudioMessage(senderID);
+        //         break;
+        //
+        //     case 'video':
+        //         sendVideoMessage(senderID);
+        //         break;
+        //
+        //     case 'file':
+        //         sendFileMessage(senderID);
+        //         break;
+        //
+        //     case 'button':
+        //         sendButtonMessage(senderID);
+        //         break;
+        //
+        //     case 'generic':
+        //         sendGenericMessage(senderID);
+        //         break;
+        //
+        //     case 'receipt':
+        //         sendReceiptMessage(senderID);
+        //         break;
+        //
+        //     case 'quick reply':
+        //         sendQuickReply(senderID);
+        //         break;
+        //
+        //     case 'read receipt':
+        //         sendReadReceipt(senderID);
+        //         break;
+        //
+        //     case 'typing on':
+        //         sendTypingOn(senderID);
+        //         break;
+        //
+        //     case 'typing off':
+        //         sendTypingOff(senderID);
+        //         break;
+        //
+        //     case 'account linking':
+        //         sendAccountLinking(senderID);
+        //         break;
+        //
+        //     default:
+        //         sendTextMessage(senderID, messageText);
+        // }
+    // } else if (messageAttachments) {
+    //     sendTextMessage(senderID, "Message with attachment received");
+    // }
 }
 
 
