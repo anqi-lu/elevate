2513f5d0006a5672351cece1bf41a48e5109afb8
Yang Liu <byliuyang11@gmail.com>
2017-09-16 19:59:50 -0400

    Remove config file

--- app.js
+++ app.js
@@ -11,7 +11,6 @@
 'use strict';
 
 const bodyParser = require('body-parser'),
-    config = require('config'),
     crypto = require('crypto'),
     express = require('express'),
     https = require('https'),
@@ -22,32 +21,18 @@ app.set('port', process.env.PORT || 5000);
 app.use(bodyParser.json({verify: verifyRequestSignature}));
 app.use(express.static('public'));
 
-/*
- * Be sure to setup your config values before running this code. You can
- * set them using environment variables or modifying the config file in /config.
- *
- */
-
 // App Secret can be retrieved from the App Dashboard
-const APP_SECRET = (process.env.MESSENGER_APP_SECRET) ?
-    process.env.MESSENGER_APP_SECRET :
-    config.get('appSecret');
+const APP_SECRET = process.env.MESSENGER_APP_SECRET;
 
 // Arbitrary value used to validate a webhook
-const VALIDATION_TOKEN = (process.env.MESSENGER_VALIDATION_TOKEN) ?
-    (process.env.MESSENGER_VALIDATION_TOKEN) :
-    config.get('validationToken');
+const VALIDATION_TOKEN = process.env.MESSENGER_VALIDATION_TOKEN;
 
 // Generate a page access token for your page from the App Dashboard
-const PAGE_ACCESS_TOKEN = (process.env.MESSENGER_PAGE_ACCESS_TOKEN) ?
-    (process.env.MESSENGER_PAGE_ACCESS_TOKEN) :
-    config.get('pageAccessToken');
+const PAGE_ACCESS_TOKEN = process.env.MESSENGER_PAGE_ACCESS_TOKEN;
 
 // URL where the app is running (include protocol). Used to point to scripts and
 // assets located at this address.
-const SERVER_URL = (process.env.SERVER_URL) ?
-    (process.env.SERVER_URL) :
-    config.get('serverURL');
+const SERVER_URL = process.env.SERVER_URL;
 
 if (!(APP_SECRET && VALIDATION_TOKEN && PAGE_ACCESS_TOKEN && SERVER_URL)) {
     console.error("Missing config values");
