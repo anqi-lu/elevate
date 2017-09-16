1f74dd9bf7f4b37bf012913480b523567c6d396e
Ravi Rahman <ravi@ravirahman.com>
2017-09-16 16:09:46 -0400

    Working on mongoose models

---
+++ models/user.js
@@ -0,0 +1,12 @@
+﻿const mongoose = require('mongoose');
+module.exports = mongoose.model('User', {
+    name: {
+        type: String,
+        required: true,
+    },
+    facebook_profile_id: {
+        type: String,
+        required: true,
+    },
+    
+});
