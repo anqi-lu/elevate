3cb6d98ba0937a0aabfcce524757786a7f7dba98
Linh Hoang <lthoang@wpi.edu>
2017-09-17 06:37:23 -0400

    add stock prive

--- controllers/messenger.controller.js
+++ controllers/messenger.controller.js
@@ -13,6 +13,8 @@
 const config = require('config'),
     crypto = require('crypto'),
     request = require('request'),
+    robinhood = require('robinhood'),
+    place_buy_order = require('../lib/robinhood/place_buy_order.js'),
     pug = require('pug');
 
 module.exports = class MessengerController {
@@ -299,6 +301,18 @@ module.exports = class MessengerController {
                 command: /^cancel$/i,
                 action: () => {
                     this.sendTextMessage(senderID, `cancel current order`);
+                }
+            },
+            {
+                getUser: () => this.findUser(senderID),
+                //get stock price
+                command: /^price of ([0-9a-zA-Z ]+)$/i,
+                action (stockName) => {
+                      robinhood(null).quote_data(stockName, (err, res, body) => {
+                          if (err) this.sendTextMessage(senderID, `error`)
+                          const answer = body.results[0].ask_price
+                          this.sendTextMessage(senderID, `${stockName} is at ${answer}`)
+                      });
                 }
             }
         ];
